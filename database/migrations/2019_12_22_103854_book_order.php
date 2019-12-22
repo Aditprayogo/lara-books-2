faf4e161c2e2b9cb435c09615b00d7c08f63ad4b
Aditprayogo <adit.ihzar@gmail.com>
2019-12-22 17:39:18 +0700

    adding migration for book_order

---
+++ database/migrations/2019_12_22_103854_book_order.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BookOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_order', function (Blueprint $table) {
+			$table->bigIncrements('id');
+            $table->bigInteger('order_id')->unsigned();
+            $table->bigInteger('book_id')->unsigned();
+            $table->bigInteger('quantity')->unsigned()->default(1);
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('book_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_order');
+    }
+}
